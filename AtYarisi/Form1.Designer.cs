70096af1674a6b40b76f64a2020f430d22d662f3
muratvuranok <muratvuranok@hotmail.com>
2019-10-30 10:51:15 +0300

    end of the at yarışı

--- AtYarisi/Form1.Designer.cs
+++ AtYarisi/Form1.Designer.cs
@@ -36,6 +36,12 @@
             this.pc1 = new System.Windows.Forms.PictureBox();
             this.btnYarisiStart = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblFinsh = new System.Windows.Forms.Label();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.lblBilgilendirme = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.p3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pc3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pc2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pc1)).BeginInit();
@@ -104,12 +110,73 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // lblFinsh
+            // 
+            this.lblFinsh.BackColor = System.Drawing.Color.White;
+            this.lblFinsh.Location = new System.Drawing.Point(1625, 110);
+            this.lblFinsh.Name = "lblFinsh";
+            this.lblFinsh.Size = new System.Drawing.Size(10, 682);
+            this.lblFinsh.TabIndex = 6;
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSifirla.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSifirla.ForeColor = System.Drawing.Color.White;
+            this.btnSifirla.Location = new System.Drawing.Point(1034, 12);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(377, 56);
+            this.btnSifirla.TabIndex = 7;
+            this.btnSifirla.Text = "Yarışı Sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // lblBilgilendirme
+            // 
+            this.lblBilgilendirme.AutoSize = true;
+            this.lblBilgilendirme.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBilgilendirme.ForeColor = System.Drawing.Color.White;
+            this.lblBilgilendirme.Location = new System.Drawing.Point(12, 22);
+            this.lblBilgilendirme.Name = "lblBilgilendirme";
+            this.lblBilgilendirme.Size = new System.Drawing.Size(0, 46);
+            this.lblBilgilendirme.TabIndex = 8;
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(1641, 152);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(153, 237);
+            this.p1.TabIndex = 10;
+            this.p1.MouseHover += new System.EventHandler(this.p1_MouseHover);
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(1641, 407);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(153, 185);
+            this.p2.TabIndex = 11;
+            this.p2.MouseEnter += new System.EventHandler(this.p2_MouseEnter);
+            // 
+            // p3
+            // 
+            this.p3.Location = new System.Drawing.Point(1641, 612);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(153, 168);
+            this.p3.TabIndex = 11;
+            this.p3.MouseEnter += new System.EventHandler(this.p3_MouseEnter);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(1806, 794);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.lblBilgilendirme);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.lblFinsh);
             this.Controls.Add(this.btnYarisiStart);
             this.Controls.Add(this.pc3);
             this.Controls.Add(this.pc2);
@@ -122,6 +189,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pc2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pc1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,6 +202,12 @@
         private System.Windows.Forms.PictureBox pc3;
         private System.Windows.Forms.Button btnYarisiStart;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblFinsh;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.Label lblBilgilendirme;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Panel p3;
     }
 }
 
--- AtYarisi/Form1.cs
+++ AtYarisi/Form1.cs
@@ -26,6 +26,8 @@ namespace AtYarisi
             //3) sol üste köşeden label olacak ve o labelda anlık olarak hangi at önde bilgisi verilecek. örn( 1. Kulvardaki ŞahBatur önde götürüyor gibi)
 
 
+
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -33,6 +35,62 @@ namespace AtYarisi
             pc1.Left += rnd.Next(1, 20);
             pc2.Left += rnd.Next(1, 20);
             pc3.Left += rnd.Next(1, 20);
+
+
+            if (pc1.Right > pc2.Right && pc1.Right > pc3.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 1. Kulvardaki Şahbatur önde götürüyor!";
+            }
+            else if (pc2.Right > pc1.Right && pc2.Right > pc3.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 2. Kulvardaki Gülbatur önde götürüyor!";
+            }
+            else if (pc3.Right > pc1.Right && pc3.Right > pc2.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 3. Kulvardaki Canbatur önde götürüyor!";
+            }
+
+
+            if (pc1.Right >= lblFinsh.Left)
+            {
+                timer1.Stop();
+                lblBilgilendirme.Text = "Yarışı 1. Kulvardaki Şahbatur Kazandı";
+                MessageBox.Show(lblBilgilendirme.Text);
+            }
+            else if (pc2.Right >= lblFinsh.Left)
+            {
+                timer1.Stop();
+                lblBilgilendirme.Text = "Yarışı 2. Kulvardaki Gülbatur Kazandı";
+                MessageBox.Show(lblBilgilendirme.Text);
+            }
+            else if (pc3.Right >= lblFinsh.Left)
+            {
+                timer1.Stop();
+                lblBilgilendirme.Text = "Yarışı 3. Kulvardaki Canbatur Kazandı";
+                MessageBox.Show(lblBilgilendirme.Text);
+            }
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pc1.Left = pc2.Left = pc3.Left = 0;
+            lblBilgilendirme.Text = "";
+        }
+
+        private void p1_MouseHover(object sender, EventArgs e)
+        {
+            pc1.Left += 15;
+        }
+
+        private void p2_MouseEnter(object sender, EventArgs e)
+        {
+            pc2.Left += 15;
+        }
+
+        private void p3_MouseEnter(object sender, EventArgs e)
+        {
+            pc3.Left += 15;
         }
     }
 }
